9a3aa1ba495f4085f76e6b3b8de78ce62f341f59
INATECH\karthikeyan.nataraja <karthikeyan.natarajan@cmshospitality.com>
2023-11-06 08:23:06 +0530

    Added reduce inventory command and handler

---
+++ Services/CQRS/Commands/ReduceInventoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.CQRS.Commands
+{
+    public class ReduceInventoryCommand:IRequest<float>
+    {
+        public int item_id { get; set; }
+        public float qty { get; set; }
+    }
+}
---
+++ Services/CQRS/Handlers/ReduceInventoryCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using CommonLibrary.Contexts;
+using CommonLibrary.Models;
+using CommonLibrary.Repositories;
+using MediatR;
+using Services.CQRS.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.CQRS.Handlers
+{
+    public class ReduceInventoryCommandHandler : IRequestHandler<ReduceInventoryCommand, float>
+    {
+        IGenericRepository<Inventory,InventoryDBContext> _repository;
+
+        public ReduceInventoryCommandHandler(IGenericRepository<Inventory, InventoryDBContext> _repository)
+        {
+            this._repository = _repository;
+        }
+
+        public async Task<float> Handle(ReduceInventoryCommand request, CancellationToken cancellationToken)
+        {
+            var currentInventory = await _repository.GetById(request.item_id);
+
+            if (currentInventory.qty >= request.qty)
+            {
+                _repository.Update(new Inventory() { id = currentInventory.id, itemId = currentInventory.itemId, qty = currentInventory.qty - request.qty, lastUpdated = DateTime.Now, Notes = "Inventory updated" });
+                await _repository.SaveChangesAsync();
+                return currentInventory.qty - request.qty;
+            }
+
+            return default;
+
+        }
+    }
+}
